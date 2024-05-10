92bd764f34726592c0fabb271ca5995e9bf34a90
PabloBelozcar <pablo.sbelozcar@gmail.com>
2024-05-10 19:57:49 -0300

    login arreglado

--- js/login.js
+++ js/login.js
@@ -2,11 +2,13 @@ let form=document.querySelector("#form");
 form.addEventListener("submit", agregar);
 
 function agregar(e){
+    e.preventDefault();
+    console.log("asdas  ")
 let formData= new FormData(form);
 let email=formData.get("email");
 let usuario=formData.get("usuario");
 let password=formData.get("password");
-console.log(email.usuario.password);
+console.log(email,usuario,password);
 }
 
-console.log(email.usuario.password);
+//console.log(email.usuario.password);
