bc971358bd1f98a310f15c5eadb26343447deca9
PabloBelozcar <pablo.sbelozcar@gmail.com>
2024-05-22 12:26:58 -0300

    a

--- js/login.js
+++ js/login.js
@@ -3,7 +3,6 @@ form.addEventListener("submit", agregar);
 
 function agregar(e){
     e.preventDefault();
-    console.log("asdas  ")
 let formData= new FormData(form);
 let email=formData.get("email");
 let usuario=formData.get("usuario");
