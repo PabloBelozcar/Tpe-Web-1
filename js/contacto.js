a8c1c25feb3c17aee54a0fbc9f27efccf6a2ee98
PabloBelozcar <pablo.sbelozcar@gmail.com>
2024-06-22 17:36:46 -0300

    a

--- js/contacto.js
+++ js/contacto.js
@@ -3,9 +3,9 @@ form.addEventListener("submit", agregar);
 
 function agregar(e){
     e.preventDefault();
-let formData= new FormData(form);
-let email=formData.get("email");
-let usuario=formData.get("usuario");
-let msj=formData.get("msj-usuario");
-console.log(email,usuario,msj);
-}
+    let formData= new FormData(form);
+    let email=formData.get("email");
+    let usuario=formData.get("usuario");
+    let msj=formData.get("msj-usuario");
+    console.log(email,usuario,msj);
+    }
