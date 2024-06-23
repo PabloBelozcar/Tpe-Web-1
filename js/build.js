1d038483e569892ea980793acb69c8d34d12ccce
PabloBelozcar <pablo.sbelozcar@gmail.com>
2024-06-23 01:56:01 -0300

    agregar echo

--- js/build.js
+++ js/build.js
@@ -1,3 +1,5 @@
+/*BLOQUE LLENAR TABLA*/ 
+
 async function ObtenerDatos(){
    const url='https://66772ee7145714a1bd740614.mockapi.io/api/Build';
    const tabla= document.querySelector("#body")
@@ -6,12 +8,61 @@ async function ObtenerDatos(){
         let respuesta= await fetch(url);
         let json= await respuesta.json();
         console.log(json);
-        for (const build of json) {
-            tabla.innerHTML+=`<tr><td>${build.nombre}</td><td>${build.numero}</td><td>${build.ubicacion}</td><td><button>borrar</button></td><td><button>editar</button></td></tr>`;
-        }
+        agregarFilas(json)
     }catch(error){
         console.log(error);
     }
 }
 
+function agregarFilas(data){
+    const tabla= document.querySelector("#body")
+
+    for (const build of data) {
+        tabla.innerHTML+=   `<tr>
+                                <td>${build.nombre}</td>
+                                <td>${build.numero}</td>
+                                <td>${build.ubicacion}</td>
+                            </tr>`;
+    }
+}
+
 ObtenerDatos();
+
+/*BLOQUE AGREGAR*/ 
+
+let formAgregar=document.querySelector("#form-agregar");
+
+formAgregar.addEventListener("submit", agregar);
+
+
+async function agregar(e){
+        e.preventDefault()
+        let formData= new FormData(formAgregar);
+        let nombre=formData.get("nombre-input");
+        let numero=formData.get("nivel-input");
+        let ubicacion=formData.get("lugar-input");
+        const url='https://66772ee7145714a1bd740614.mockapi.io/api/Build';
+        let item={
+            "nombre": nombre,
+            "numero": numero,
+            "ubicacion": ubicacion
+        }
+
+        try {
+            let res= await fetch(url, {
+                "method": "POST",
+                "headers": {"Content-type": "application/json"},
+                "body": JSON.stringify(item)
+            });
+            if (res === 201){
+                agregarFilas(item)
+                console.log("se pudio")
+            }
+        } catch (error) {
+            console.log(error)
+        }
+
+}
+
+
+
