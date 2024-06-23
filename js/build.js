3c8257f8912367390cc2d8f4140448a6fc23ff62
PabloBelozcar <pablo.sbelozcar@gmail.com>
2024-06-23 16:14:07 -0300

    agregar y borrar echo perfeto creo

--- js/build.js
+++ js/build.js
@@ -1,30 +1,32 @@
+const url='https://66772ee7145714a1bd740614.mockapi.io/api/Build';
 /*BLOQUE LLENAR TABLA*/ 
 
 async function ObtenerDatos(){
-   const url='https://66772ee7145714a1bd740614.mockapi.io/api/Build';
    const tabla= document.querySelector("#body")
    tabla.innerHTML="";
      try{
         let respuesta= await fetch(url);
         let json= await respuesta.json();
         console.log(json);
-        agregarFilas(json)
+        agregarFilas(json);
     }catch(error){
         console.log(error);
     }
 }
 
-function agregarFilas(data){
+function agregarFilas(json){
     const tabla= document.querySelector("#body")
 
-    for (const build of data) {
+    for (const build of json) {
         tabla.innerHTML+=   `<tr>
+                                <td>${build.id}</td>
                                 <td>${build.nombre}</td>
                                 <td>${build.numero}</td>
                                 <td>${build.ubicacion}</td>
                             </tr>`;
     }
 }
+
 
 ObtenerDatos();
 
@@ -41,7 +43,6 @@ async function agregar(e){
         let nombre=formData.get("nombre-input");
         let numero=formData.get("nivel-input");
         let ubicacion=formData.get("lugar-input");
-        const url='https://66772ee7145714a1bd740614.mockapi.io/api/Build';
         let item={
             "nombre": nombre,
             "numero": numero,
@@ -54,15 +55,54 @@ async function agregar(e){
                 "headers": {"Content-type": "application/json"},
                 "body": JSON.stringify(item)
             });
-            if (res === 201){
-                agregarFilas(item)
-                console.log("se pudio")
+            if (res.status === 201){
+                ObtenerDatos()
             }
         } catch (error) {
             console.log(error)
         }
 
 }
+
+
+/*BLOQUE BORRAR*/
+
+let btnBorrar=document.querySelector(".btn-borrar");
+
+btnBorrar.addEventListener("click", borrar)
+
+async function borrar(){
+    let idImput=document.getElementById("id-item")
+    const id=idImput.value
+    try {
+        let respuesta= await fetch(url);
+        let json= await respuesta.json();
+        const itemValido=buscarID(json,id)
+        if (itemValido != null){
+            let res= await fetch(`${url}/${id}`,{
+                "method": "DELETE"
+            });
+            if (res.status === 200){
+                ObtenerDatos()
+                document.querySelector("#aviso").innerHTML = "Eliminado!"
+            }
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+function buscarID(json, id){
+    for (const item of json){
+        if (item.id === id){
+            return item;
+        }
+    }
+    return null;
+}
+
 
 
 
